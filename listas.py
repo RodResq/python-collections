a410a1370500132daeac7a364b1951121a088133
RodResq <rodrigoresque@yahoo.com.br>
2021-09-18 11:27:38 -0300

    updt

---
+++ listas.py
@@ -0,0 +1,7 @@
+lista_simples_inteiros = [1, 2, 3, 4, 5]
+lista_simples_string = ["Ola", "Mundo"]
+lista_simples_mescladas = [1, 2, 3, "Ola Mundo", True, 1.5]
+nested_list = [[1, 2, True], ["Ola", "Mundo"]]
+
+print(lista_simples_inteiros)
+print(nested_list)
