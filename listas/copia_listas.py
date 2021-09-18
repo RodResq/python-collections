7e51e5b5f6edbbda6dcd7eb4515531f379b512bb
RodResq <rodrigoresque@yahoo.com.br>
2021-09-18 15:52:23 -0300

    updt

---
+++ listas/copia_listas.py
@@ -0,0 +1,16 @@
+import copy
+
+nested_list = [[1, 2, True], ["Ola", "Mundo"]]
+
+# Deep Copy
+nova_lista = copy.deepcopy(nested_list)
+nested_list[0][1] = "A"
+print(nova_lista)
+print(nested_list)
+
+# Shallow Copy
+outra_lista = copy.copy(nested_list)
+nested_list[0][1] = "B"
+print(outra_lista)
+print(nested_list)
+
