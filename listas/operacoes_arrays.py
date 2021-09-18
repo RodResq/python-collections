42aee99c4aad7732a523e7c50105e0d349e5db1a
RodResq <rodrigoresque@yahoo.com.br>
2021-09-18 16:34:14 -0300

    updt

--- listas/operacoes_arrays.py
+++ listas/operacoes_arrays.py
@@ -1,3 +1,34 @@
 from array import array
 
-array_1 = array()
+array_1 = array('B', [1, 2, 3, 4, 5, 6])
+
+array_2 = array('u', ['O', 'L', 'A'])
+
+
+for i in array_2:
+    print(i)
+
+# print(array_1)
+#
+# for i in array_1:
+#     print(i)
+
+# Inserir elementos no Array
+array_1.insert(2, 50)
+# for i in array_1:
+#     print(i)
+
+# Removendo Elementos do array
+array_1.remove(4)
+#
+# for i in array_1:
+#     print(i)
+
+# Buscar elemento em um array
+# print(array_1.index(50))
+
+# Atualizar dados em um array
+array_1[2] = 55
+
+for i in array_1:
+    print(i)
