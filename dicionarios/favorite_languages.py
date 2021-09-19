c15505b69b0871c3b37eacb2f455d2ccf944dd52
RodResq <rodrigoresque@yahoo.com.br>
2021-09-19 11:38:49 -0300

    updt

--- dicionarios/favorite_languages.py
+++ dicionarios/favorite_languages.py
@@ -11,3 +11,10 @@ for name in favorite_language.keys():
     print(name.title())
     if name in friends:
         print(f" Hi {name.title()}, \n I see your favorite language is {favorite_language.get(name).title()}." )
+
+if 'erin' not in favorite_language.keys():
+    print('Erin, please take our poll')
+
+print("Linguagems: ")
+for language in sorted(set((favorite_language.values()))):
+    print(f"{language}")
